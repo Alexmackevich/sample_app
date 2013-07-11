9fa828f5a39b2577bfba9556d35bc1331f33288e
Alex <41exmackevich@gmail.com>
2013-07-11 21:06:13 +0300

    add static_pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'd6dfb58b3ecb0e65f800e76bae2ddb0a3b5ccaf3f4cbabf00ea6b475d10677e8f8de04ef997e590cd713f2098346be7b524fca6375a9fc00694c27dbc846e534'
